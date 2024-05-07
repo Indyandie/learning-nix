b014a587fb4ec940514cd7f23b3237722b8767cc
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-05-07 00:29:18 -0500

    refactor(mkShell): add comments and minor changes

--- nix-shell/shell.nix
+++ nix-shell/shell.nix
@@ -1,18 +1,22 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   # pkgs.mkShellNoCC { # if no C compiler is needed
-  name = "nx-zsh";
 
+  # (default: nix-shell). Set the name of the derivation.
+  name = "test shell";
+
+  # (default: []). Add executable packages to the nix-shell environment.
   packages = [
     pkgs.zsh
   ];
 
-  inputsFrom = [
-    # pkgs.zsh
-  ];
+  # (default: []). Add build dependencies of the listed derivations to the nix-shell environment.
+  inputsFrom = [ ];
 
+  # (default: ""). Bash statements that are executed by nix-shell.
   shellHook = ''
-    zsh # start zsh shell
+    zsh -l # start zsh shell
     export NIX_SHELL_TEST="This is a test env var"
+    echo $NIX_SHELL_TEST
   '';
 }
